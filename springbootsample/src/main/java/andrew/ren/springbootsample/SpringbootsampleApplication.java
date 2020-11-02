cd4e79e3fc5b230b05014c6e201deed66c183a68
lazydragon <lazy.dragon@hotmail.com>
2020-11-02 01:58:07 +0000

    init build with spring boot

---
+++ springbootsample/src/main/java/andrew/ren/springbootsample/SpringbootsampleApplication.java
@@ -0,0 +1,13 @@
+package andrew.ren.springbootsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootsampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootsampleApplication.class, args);
+	}
+
+}
